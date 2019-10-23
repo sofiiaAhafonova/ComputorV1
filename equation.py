49dd3e1da86c71f2b1b5d48c453dacdeeadbe643
Sofiia Ahafonova <sofiia@bitcorp.ai>
2019-10-23 13:48:02 +0300

    Handle higher degrees

--- equation.py
+++ equation.py
@@ -1,14 +1,21 @@
-from math_func import ft_abs, ft_sqrt, ft_pow
+from math_func import ft_abs, ft_sqrt
+import re
 
 
 class Equation(object):
     allowed_symbols = ['+', '-', '*', '=']
+    pattern = r'^X\^\d+$'
+    max_degree = 10
 
     def __init__(self, str_equation):
-        self.x = [0] * 3
+        self.x = [0] * (self.max_degree + 1)
         self.solver = [self._zero_degree, self._linear, self._quadratic]
         self.degree = 0
-        self._parse(str_equation)
+        try:
+            self._parse(str_equation)
+            self._solve()
+        except ValueError:
+            print('Invalid equation')
 
     def _parse(self, str_equation: str):
         for s in self.allowed_symbols:
@@ -20,26 +27,32 @@ class Equation(object):
             e = equation[i]
             if e.isdigit() or e.replace('.', '', 1).isdigit():
                 mult = float(e) * sign
-                if equation[i + 1] != '*' or equation[i + 2] not in ['X^0', 'X^1', 'X^2']:
-                    raise ValueError('Invalid input')
-                e = equation[i + 2]
-                for j in range(3):
-                    if e.count(str(j)):
-                        self.x[j] += mult
-                        break
+                if equation[i + 1] != '*' or not re.match(self.pattern, equation[i + 2]):
+                    raise ValueError()
+                e = equation[i + 2][2:]
+                if not e.isdigit():
+                    raise ValueError()
+                j = int(e)
+                self.degree = j if j > self.degree and mult else self.degree
+                if self.degree > self.max_degree:
+                    break
+                self.x[j] += mult
                 i += 2
             elif e in ['-', '+']:
                 sign *= -1 if e == '-' else 1
             elif e == '=':
                 sign = -1
-            elif e in ['X^0', 'X^1', 'X^2']:
-                for j in range(3):
-                    if e.count(str(j)):
-                        self.x[j] += 1
-                        break
+            elif re.match(self.pattern, e):
+                e = e[2:]
+                if not e.isdigit():
+                    raise ValueError()
+                j = int(e)
+                self.degree = j if j > self.degree else self.degree
+                if self.degree > self.max_degree:
+                    break
+                self.x[j] += 1
             else:
-                print(e)
-                raise ValueError('Invalid input')
+                raise ValueError()
             i += 1
 
     def _print_sign(self, x):
@@ -47,9 +60,9 @@ class Equation(object):
 
     def _reduce(self):
         self._count_degree()
-        res = f'{self.x[0]} * X^0'
+        res = f'{self.x[0]:g} * X^0'
         for i in range(1, self.degree + 1):
-            res += f' {self._print_sign(self.x[i])} {ft_abs(self.x[i])} * X^{i}'
+            res += f' {self._print_sign(self.x[i])} {ft_abs(self.x[i]):g} * X^{i}'
         return res + ' = 0'
 
     def __str__(self):
@@ -64,8 +77,8 @@ class Equation(object):
             print(-self.x[1] / (2 * self.x[2]))
         else:
             if discriminant < 0:
-                i = ft_sqrt(-discriminant) / (2 * self.x[2])
                 print("Discriminant is strictly negative, the two solutions are:")
+                i = ft_sqrt(-discriminant) / (2 * self.x[2])
                 x_1 = (-self.x[1] / (2 * self.x[2]), self._print_sign(-i), ft_abs(i))
                 x_2 = (-self.x[1] / (2 * self.x[2]), self._print_sign(i), ft_abs(i))
                 print("\033[34m(-b - (d ** 0.5)) / (2 * a) = \033[32m{:g} {} {:g} * i\033[39m"
@@ -101,7 +114,7 @@ class Equation(object):
         while not self.x[self.degree]:
             self.degree -= 1
 
-    def solve(self):
+    def _solve(self):
         self._print_reduce()
         self._print_degree()
         if self.degree > 2:
@@ -109,13 +122,12 @@ class Equation(object):
         else:
             self.solver[self.degree]()
 
-"""
-eq = Equation('5 * X^0 + 4 * X^1 - 9.3 * X^2 =1 * X^0')
-eq.solve()
+
+Equation('5 * X^0 + 4 * X^1 - 9.3 * X^2 + 1 * X^3 =1 * X^0')
+
 print()
-eq1 = Equation("5 * X^0 + 4 * X^1 = 4 * X^0")
-eq1.solve()
+Equation("5 * X^0 + 4 * X^1 = 4 * X^0")
+
 print()
-"""
-eq = Equation('5 * X^0 + 4 * X^1 + X^2 = 0 * X^0')
-eq.solve()
+
+Equation('5 * X^0 + 4 * X^1 + X^2 = 0 * X^0')
