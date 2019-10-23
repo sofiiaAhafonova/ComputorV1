19918a083aa387fd2f87413bda23e0459d7afc2c
Sofiia Ahafonova <sofiia@bitcorp.ai>
2019-10-23 12:21:34 +0300

    Solve 2 degree equation(partial)

--- equation.py
+++ equation.py
@@ -1,4 +1,4 @@
-from math_func import ft_abs
+from math_func import ft_abs, ft_sqrt, ft_pow
 
 
 class Equation(object):
@@ -6,9 +6,13 @@ class Equation(object):
 
     def __init__(self, str_equation):
         self.x = [0] * 3
+        self.solver = [self._zero_degree, self._linear, self._quadratic]
+        self.degree = 0
         self._parse(str_equation)
 
     def _parse(self, str_equation: str):
+        for s in self.allowed_symbols:
+            str_equation = str_equation.replace(s, f' {s} ')
         equation = str_equation.split()
         mult, sign = 1, 1
         i = 0
@@ -36,14 +40,63 @@ class Equation(object):
     def _reduce(self):
         def sign(x):
             return '-' if x < 0 else '+'
-
-        return f'{self.x[0]} * X^0 {sign(self.x[0])} {ft_abs(self.x[1])} * X^1 ' \
-               f'{sign(self.x[2])} {ft_abs(self.x[2])} * X^2 = 0'
+        self._count_degree()
+        res = f'{self.x[0]} * X^0'
+        for i in range(1, self.degree + 1):
+            res += f' {sign(self.x[i])} {ft_abs(self.x[i])} * X^{i}'
+        return res + ' = 0'
 
     def __str__(self):
         return self._reduce()
 
+    def _quadratic(self):
+        discriminant = self.x[1] * self.x[1] - 4 * self.x[2] * self.x[1]
+        print(f'Discriminant is {discriminant}')
+        if discriminant < 0:
+            print("Discriminant is strictly negative, the two solutions are:")
+        elif discriminant == 0:
+            print("Discriminant is 0, the solution is:")
+            print(-self.x[1] / (2 * self.x[2]))
+        else:
+            print("Discriminant is strictly positive, the two solutions are:")
+            x_1 = (-self.x[1] - ft_sqrt(discriminant)) / (2 * self.x[2])
+            x_2 = (-self.x[1] + ft_sqrt(discriminant)) / (2 * self.x[2])
+            print("\033[34m(-b - (d ** 0.5)) / (2 * a) = \033[32m{0:.6f}\033[39m".format(x_1))
+            print("\033[34m(-b + (d ** 0.5)) / (2 * a) = \033[32m{0:.6f}\033[39m".format(x_2))
 
-eq = Equation('5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0')
-print(eq)
-print(Equation("5 * X^0 + 4 * X^1 = 4 * X^0"))
+    def _linear(self):
+        res = - self.x[0] / self.x[1]
+        print('The solution is:')
+        print(res)
+
+    def _zero_degree(self):
+        if self.x[0] == 0:
+            print('All the real numbers are solution')
+        else:
+            print('There is no solution of equation')
+
+    def _print_degree(self):
+        print(f'Polynomial degree: {self.degree}')
+
+    def _print_reduce(self):
+        print(f'Reduced form: {self._reduce()}')
+
+    def _count_degree(self):
+        self.degree = len(self.x) - 1
+        while not self.x[self.degree]:
+            self.degree -= 1
+
+    def solve(self):
+        self._print_reduce()
+        self._print_degree()
+        if self.degree > 2:
+            print('The polynomial degree is strictly greater than 2, I can\'t solve')
+        else:
+            self.solver[self.degree]()
+
+
+eq = Equation('5 * X^0 + 4 * X^1 - 9.3 * X^2 =1 * X^0')
+eq.solve()
+print()
+eq1 = Equation("5 * X^0 + 4 * X^1 = 4 * X^0")
+eq1.solve()
--- math_func.py
+++ math_func.py
@@ -1,26 +1,8 @@
 import sys
 
+
 def ft_abs(a):
     return a if a >= 0 else -a
-
-
-def int_div(dividend, divisor):
-    sign = -1 if ((dividend < 0) ^ (divisor < 0)) else 1
-    dividend = ft_abs(dividend)
-    divisor = ft_abs(divisor)
-    quotient = 0
-    while dividend >= divisor:
-        dividend -= divisor
-        quotient += 1
-    return sign * quotient
-
-
-def float_div(dividend, divisor):
-    pass
-
-
-def ft_div(dividend, divisor):
-    pass
 
 
 def ft_mod(dividend, divisor):
@@ -63,16 +45,3 @@ def ft_sqrt(n):
         elif i * i > n:
             return float_sqrt(n, i - 1, i)
         i += 1
-
-
-def main():
-    av = sys.argv
-    ac = len(av)
-    print(av, ac)
-    import math
-    print(ft_sqrt(211111111111111.5))
-    print(math.sqrt(2111111111111.5))
-
-
-if __name__ == '__main__':
-    main()
